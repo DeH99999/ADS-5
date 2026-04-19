e83a8d3fc97168169a0842ebe752d272b2462b92
DeH99999 <vakasin-denis@mail.ru>
2026-04-19 22:10:27 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,32 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int Capacity>
 class TStack {
-  // добавьте код стека
+ private:
+  T buffer[Capacity];
+  int topIndex;
+
+ public:
+  TStack() : topIndex(-1) {}
+
+  void add(const T& item) {
+    if (topIndex < Capacity - 1)
+      buffer[++topIndex] = item;
+  }
+
+  void remove() {
+    if (topIndex >= 0)
+      --topIndex;
+  }
+
+  T peek() const {
+    return buffer[topIndex];
+  }
+
+  bool isVacant() const {
+    return topIndex == -1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
